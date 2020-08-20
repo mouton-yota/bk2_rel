0e99483e787285ed3773e9c4119230e4af0a3bd6
vagrant <vagrant@localhost.localdomain>
2020-08-20 13:51:53 +0000

    [add]20200820

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about'
 
+
   devise_for :users
   resources :users, :only => [:show,:index,:edit,:update]
   resources :books do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
-
 
 end
